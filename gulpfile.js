7050111c5321be73d6f7a21d9342a8d79511db98
Lev Khomchenkov <pokemon9116@yandex.ru>
2020-07-16 17:13:55 +0700

    almost start app !!!

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+let gulp = require('gulp')
+let sass = require('gulp-sass')
+let browserSync = require('browser-sync')
+
+function sassTask(name){
+    gulp.task('sass', function(){                                               
+        return gulp.src(`pages/${name}/${name}.+(scss|sass)`)                    
+            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))   
+            .pipe(gulp.dest(`pages/${name}`))                          
+            .pipe(browserSync.reload({stream: true}))                           
+    })
+
+    gulp.task('browser-sync', function() {                                     
+        browserSync({                                                          
+            server: {                                                           
+                baseDir: `pages/${name}`                                  
+            },
+            notify: false                                                        
+        });
+    });
+} 
+
+function updateTask(name){
+    gulp.task('scripts', function() {
+        return gulp.src(`pages/scripts/*.js`)
+        .pipe(browserSync.reload({ stream: true }))
+    });
+
+    gulp.task('code', function() {
+        return gulp.src(`pages/${name}/index.html`)
+        .pipe(browserSync.reload({ stream: true }))
+    });
+
+    gulp.task('watch', function() {
+        gulp.watch(`pages/${name}/${name}.+(scss|sass)`, gulp.parallel('sass')); 
+        gulp.watch(`pages/${name}/index.html`, gulp.parallel('code'));         
+        gulp.watch(`pages/scripts/*.js`, gulp.parallel('scripts'));     
+    });
+
+    gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+}
+
+sassTask("main_page")
+updateTask("main_page")
